4eb8b3974d04f481b50d34f300e7722a276159e9
toandvph19458 <105204332+toandvph19458@users.noreply.github.com>
2023-09-12 09:22:25 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// let const - block scoped variables
+// not hoisting
+// camelCase
+
+// Kieu du lieu (primitive/non-primitive)(value types/ reference types)
+// string, number, boolean(true/false), null, undefined, object, array, function
+// primitive: string, number, boolean, null, undefined
+// non-primitive: object, array, function
