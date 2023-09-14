24c5c6863d13e7e9e318de990ba756587aab0cc5
toandvph19458 <105204332+toandvph19458@users.noreply.github.com>
2023-09-14 11:52:52 +0700

    baitap

--- index.js
+++ index.js
@@ -65,16 +65,15 @@ let result = diemDatDuoc > diemTB ? "Pass roi" : "chua Pass";
 const array = ["Duc", "Tung", "Nguyen", "Vuong", "Toan"];
 
 array.forEach(function (item, index) {
-	console.log("forEach item", item);
-	console.log("forEach index", index);
+
 });
 console.log("==========");
 for (let item in array) {
-	console.log("forIn result", item);
+
 }
 console.log("==========");
 for (let item of array) {
-	console.log("forOf result", item);
+
 }
 
 // if/else statement
@@ -104,3 +103,131 @@ switch (day) {
 		console.log("ngay lv");
 		break;
 }
+// tao 1 vong lap for lap lai 10 lan, trong moi lan thi` bien status (boolean) se thay doi nguoc lai voi chinh no, console.log ra
+
+
+// Buoi 2:
+
+// ======================Object======================
+// Cach khai bao object
+// Cach 1: su dung  new Object()
+
+// const employee = new Object();
+
+// console.log("employee: ",employee);
+
+class Human{
+	constructor(nameParam, heightParam, isSingleParam){
+		this.name = nameParam;
+		this.height = heightParam,
+		this.isSingle = isSingleParam;
+		this.work = function(){
+			console.log("I'm working");
+		}
+	}
+	sleep(){
+		console.log("I'm sleeping");
+	}
+	introduce(){
+		console.log("Toi la "+this.name+", toi cao "+this.height+" cm");
+	}
+}
+
+const toan = new Human("Toan","300",true);
+
+toan.introduce();
+// Cach2: dung cu phap new Object()
+const newPlainObject = new Object();
+newPlainObject.age = 24;
+
+// Cach 3:
+const oldObject = {
+	name: "toan",
+	age: 21 
+};
+  //Shallow copy: khi thay doi gia tri cua object moi thi object cu cung bi thay doi
+
+// immutable ===> Deep copy
+// c1: dung Object.assign()
+
+const clone1 = Object.assign({}, oldObject);
+clone1.name = "Tung";
+
+console.log("clone 1: ",clone1);
+console.log("old object: ", oldObject);
+
+// c2: 
+
+const clone2 = JSON.parse(JSON.stringify(oldObject));
+console.log("clone 2: ", clone2);
+
+// ======================Function======================
+
+// function declaration | default params
+
+function fnName(param1 = 1, param2 = 2){
+	return param1 + param2;
+}
+console.log(fnName());
+// function fn1 () {
+// 	return "Hello"
+// }
+// function fn2 (fnameParam, lnameParam) {
+// 	return "ten toi la: "+ fnameParam + lnameParam;
+// }
+
+// function fn3 () {
+	
+// }
+// function fn4 (fname, lname) {
+// 	console.log(fname + lname);
+// }
+
+// function expression
+
+const fnName2 = function(){
+
+}
+
+// arrow function | ECMA Script 6
+
+// const fn3 = (param1, param2) => {
+
+// }
+
+// const sum = (a,b) => {
+// 	return a+b
+// }
+
+// goi ham: fnName(param1, param2)
+
+// IIFE | chi chay dung 1 lan, k the goi lai
+
+// (function(a,b){
+// 	console.log(a+b)
+// })(1,2);
+
+// ((a,b)=>{
+// 	console.log(a+b)
+// })(3,4);
+
+// ******Callback fn: mot ham` truyen vao mot ham` khac nhu mot param,  
+
+// ["Toan","Vuong","Nguyen","Tung"].forEach(function(item, index){
+// 	console.log("Toi ten la: ", item);
+// 	console.log("Index: ", index);
+// })
+
+// turn 1: (Toan, 0) ==> callback(Toan,0)
+function sum (a,b){
+	return console.log(a+b);
+}
+function div (a,b){
+	return console.log(a-b);
+
+}
+function abc(a,b,callback){
+	return callback(a,b)
+}
+abc(1,2,sum);
+abc(1,2,div);
