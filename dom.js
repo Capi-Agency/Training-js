b36acfc322b5d893cccac4c673f159594a7c7cc0
Nhân Vượng <52657226+vuonggluon@users.noreply.github.com>
2023-09-23 16:10:34 +0700

    Update delete button

--- dom.js
+++ dom.js
@@ -28,34 +28,25 @@
 //     menuNav.classList.toggle('hide')
 // })
 
-
 /** Tạo danh sách:
-
-Tạo một danh sách <ul> trong HTML với một ID như "myList".
-Tạo một nút button với ID "addItemButton".
-Khi người dùng nhấn vào nút "addItemButton", thêm một mục mới vào danh sách.
-
-Xử lý sự kiện:
-
-Tạo một ô input với một ID như "myInput".
-Tạo một nút button với ID "submitButton".
-Khi người dùng nhập thông tin vào input và nhấn nút "submitButton", hiển thị thông báo "Đã nhấn submit" trên trang.
- */
-
-
-
-
-/** Tạo danh sách:
-Tạo một danh sách <ul> trong HTML với một ID như "myList".
-Tạo một nút button với ID "addItemButton".
-Khi người dùng nhấn vào nút "addItemButton", thêm một mục mới vào danh sách.
+ Tạo một danh sách <ul> trong HTML với một ID như "myList".
+ Tạo một nút button với ID "addItemButton".
+ Khi người dùng nhấn vào nút "addItemButton", thêm một mục mới vào danh sách.
  */
 
 let addBtn = document.querySelector('#addItemButton')
+let deleteBtn = document.querySelector('#deleteItemButton')
 let itemValue = document.querySelector('#myList')
+let quantity = 0
 
 addBtn.addEventListener('click', function () {
-    itemValue.innerHTML += '<li>item</li>'
+    quantity += 1
+    itemValue.innerHTML += '<li>item '+ quantity +'</li>'
+})
+
+deleteBtn.addEventListener('click', function () {
+    quantity -= 1
+    itemValue.removeChild(itemValue.lastElementChild)
 })
 
 /** Xử lý sự kiện:
