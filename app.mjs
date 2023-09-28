940abf1d15dfd7da492b80a5ad5408db45965bee
tunggg <tranhuutungkeu@gmail.com>
2023-09-28 12:04:30 +0700

    buoi 6

---
+++ app.mjs
@@ -0,0 +1,8 @@
+// export const fruit = ["banana","coconut"];
+// const vegetables = ["carbage", "rau cai"];
+// export {vegetables};
+export const a = 3;
+const b = 2;
+export {b};
+const c = 2;
+export default c;
